e5c1544c17ad134c6945503004e9e8c8a27ba34c
Julio.S <just.julio95@gmail.com>
2022-05-25 14:13:49 -0400

    worked on the sample database a bit, ensuring it all works as expected.

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,25 @@
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-)
+    name VARCHAR(30) NOT NULL
+);
 
 CREATE TABLE roles (
-    id INTEGER AUTO_INCREMENT PRIMARY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(4,2) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
     department_id INTEGER,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employees (
-
-)
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
+);
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO departments (name) VALUES
+    ('Sales'),
+    ('Human Resources'),
+    ('Management'),
+    ('Publicity');
+
+INSERT INTO roles (title, salary, department_id) VALUES
+    (),
+    (),
+    (),
